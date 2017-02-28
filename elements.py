e8531455d1d8a54729584ac317900a0006546116
thisdotrob <robstevenson000@gmail.com>
2017-02-28 08:01:10 +0000

    Replace element ID magic strings with constants class

---
+++ elements.py
@@ -0,0 +1,4 @@
+class ElementIds:
+    COUNTRY_INPUT = "countryName"
+    PAY_MONTHLY_BUTTON = "paymonthly"
+    CALL_RATES_TABLE = "standardRatesTable"
--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from elements import ElementIds
 
 ENTRY_PAGE_URL = "http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk"
 
@@ -7,11 +8,11 @@ def get_landline_rate(country):
     driver = webdriver.Chrome()
     driver.implicitly_wait(5)
     driver.get(ENTRY_PAGE_URL)
-    country_input = driver.find_element_by_id("countryName")
+    country_input = driver.find_element_by_id(ElementIds.COUNTRY_INPUT)
     country_input.send_keys(country + Keys.RETURN)
-    pay_monthly_button = driver.find_element_by_id("paymonthly")
+    pay_monthly_button = driver.find_element_by_id(ElementIds.PAY_MONTHLY_BUTTON)
     pay_monthly_button.click()
-    call_rates_table = driver.find_element_by_id("standardRatesTable")
+    call_rates_table = driver.find_element_by_id(ElementIds.CALL_RATES_TABLE)
     landline_rate_row = call_rates_table.find_elements_by_tag_name("tr")[0]
     landline_rate = landline_rate_row.find_elements_by_tag_name("td")[1].text
 
