f11b7c68efd332b259c080d31ae700e986910882
thisdotrob <robstevenson000@gmail.com>
2017-02-28 08:21:38 +0000

    Refactor get_landline_rate into CountryScraper class

--- scrape.py
+++ scrape.py
@@ -5,18 +5,34 @@ from elements import ElementIds
 ENTRY_PAGE_URL = "http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk"
 TIMEOUT_SECS = 5
 
-def get_landline_rate(country):
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(TIMEOUT_SECS)
-    driver.get(ENTRY_PAGE_URL)
-    country_input = driver.find_element_by_id(ElementIds.COUNTRY_INPUT)
-    country_input.send_keys(country + Keys.RETURN)
-    pay_monthly_button = driver.find_element_by_id(ElementIds.PAY_MONTHLY_BUTTON)
-    pay_monthly_button.click()
-    call_rates_table = driver.find_element_by_id(ElementIds.CALL_RATES_TABLE)
-    landline_rate_row = call_rates_table.find_elements_by_tag_name("tr")[0]
-    landline_rate = landline_rate_row.find_elements_by_tag_name("td")[1].text
+class CountryScraper:
+    def __init__(self, country):
+        self.country = country
+        self._initialise_driver()
 
-    driver.close()
+    def get_landline_rate(self):
+        self._visit_entry_page()
+        self._select_country()
+        self._select_pay_monthly()
+        return self._extract_landline_rate_from_table()
 
-    return landline_rate
+    def _initialise_driver(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(TIMEOUT_SECS)
+
+    def _visit_entry_page(self):
+        self.driver.get(ENTRY_PAGE_URL)
+
+    def _select_country(self):
+        country_input = self.driver.find_element_by_id(ElementIds.COUNTRY_INPUT)
+        country_input.send_keys(self.country + Keys.RETURN)
+
+    def _select_pay_monthly(self):
+        pay_monthly_button = self.driver.find_element_by_id(ElementIds.PAY_MONTHLY_BUTTON)
+        pay_monthly_button.click()
+
+    def _extract_landline_rate_from_table(self):
+        call_rates_table = self.driver.find_element_by_id(ElementIds.CALL_RATES_TABLE)
+        landline_rate_row = call_rates_table.find_elements_by_tag_name("tr")[0]
+        landline_rate_cell = landline_rate_row.find_elements_by_tag_name("td")[1]
+        return landline_rate_cell.text
--- test/test_scrape.py
+++ test/test_scrape.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
-from scrape import get_landline_rate
+from scrape import CountryScraper
 
 class ScrapeTest(TestCase):
     def test_retrieving_landline_rate(self):
-        canada_landline_rate = get_landline_rate("Canada")
-        pakistan_landline_rate = get_landline_rate("Pakistan")
+        canada_scraper = CountryScraper("Canada")
+        pakistan_scraper = CountryScraper("Pakistan")
+        canada_landline_rate = canada_scraper.get_landline_rate()
+        pakistan_landline_rate = pakistan_scraper.get_landline_rate()
         self.assertEqual(canada_landline_rate, "£1.50")
         self.assertEqual(pakistan_landline_rate, "£2.00")
