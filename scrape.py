56ccb29f85daf90591f0c2b96b46771755a29a38
thisdotrob <robstevenson000@gmail.com>
2017-02-28 07:40:37 +0000

    Retrieve landline rate for single country

--- scrape.py
+++ scrape.py
@@ -1,8 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+ENTRY_PAGE_URL = "http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk"
 
 def get_title():
     driver = webdriver.Chrome()
-    driver.get("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk")
+    driver.get(ENTRY_PAGE_URL)
     title = driver.title
     driver.quit()
     return title
+
+def get_landline_rate(country):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    driver.get(ENTRY_PAGE_URL)
+    country_input = driver.find_element_by_id("countryName")
+    country_input.send_keys(country + Keys.RETURN)
+    pay_monthly_button = driver.find_element_by_id("paymonthly")
+    pay_monthly_button.click()
+    call_rates_table = driver.find_element_by_id("standardRatesTable")
+    landline_rate_row = call_rates_table.find_elements_by_tag_name("tr")[0]
+    landline_rate = landline_rate_row.find_elements_by_tag_name("td")[1].text
+
+    driver.close()
+
+    return landline_rate
--- test/test_scrape.py
+++ test/test_scrape.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
-from scrape import get_title
+from scrape import get_title, get_landline_rate
 
 class ScrapeTest(TestCase):
     def test_retrieving_entry_page_title(self):
         title = get_title()
         self.assertEqual(title, "O2 | International | International Caller Bolt On")
+
+    def test_retrieving_landline_rate(self):
+        canada_landline_rate = get_landline_rate("Canada")
+        pakistan_landline_rate = get_landline_rate("Pakistan")
+        self.assertEqual(canada_landline_rate, "£1.50")
+        self.assertEqual(pakistan_landline_rate, "£2.00")
